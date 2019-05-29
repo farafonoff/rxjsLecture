ec2fec734d5d546c0cc91abb2de3840a74e0f0bf
Artem Farafonov <artem_farafonov@epam.com>
2019-05-29 16:30:41 +0400

    add error handling

--- src/utils/websocket.ts
+++ src/utils/websocket.ts
@@ -2,10 +2,14 @@ import { Observable } from 'rxjs/Rx'
 import { retryWhen, switchMap } from 'rxjs/operators'
 import { timer } from 'rxjs';
 
-const socket = new WebSocket('ws://127.0.0.1:8001');
+let socket: WebSocket;
+
 const source = new Observable<MessageEvent>((observer) => {
+    socket = new WebSocket('ws://127.0.0.1:8001');
     const listener = (e: MessageEvent) => observer.next(e);
     socket.addEventListener('message', listener);
+    socket.addEventListener('close', () => observer.error('disconnect'))
+    socket.addEventListener('error', () => observer.error('disconnect'))
     return () => socket.removeEventListener('message', listener);
 }).pipe(
     retryWhen(errors => errors.pipe(
